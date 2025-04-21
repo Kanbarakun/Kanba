cfa61ab3a70bcb06776054bd5b56e9e15e8833c4
Kanbarakun <caguiclajordan1212@Gmail.com>
2025-04-22 00:14:31 +0800

    Update GradeSummary(Simplified.cpp

--- GradeSummary(Simplified.cpp
+++ GradeSummary(Simplified.cpp
@@ -7,8 +7,18 @@ int main() {
     vector<string> names;
     vector<int> math, english, science;
     char choice;
-
+    int number_of_students;
     do {
+        do{
+        cout << "How many students (1 to " << 12 - names.size() << " only): ";
+        cin >> number_of_students;
+        if(number_of_students < 1 || number_of_students > 12 - names.size())
+          cout << "Invalid You can only input up to 12 students only.\n";
+        }while(number_of_students < 1 ||number_of_students > 12 - names.size());
+
+        for(int i=0; i < number_of_students; i++)
+        {
+        cout << "\n[Student " << names.size() + 1 << "]\n";
         string name;
         int m, e, s;
 
@@ -32,22 +42,32 @@ int main() {
         cout << "Science grade (0-99): ";
         cin >> s;
         science.push_back(s);
+        }
 
-        // Ask if user wants to add another student
-        cout << "Add another student? (Y/N): ";
-        cin >> choice;
+        if(names.size() < 12){
+            cout << "\nDo you want to input more students? (Y/N): ";
+            cin >> choice;
+        }
+        else{
+            cout << "\nYou have reached the maximum amount of students.\n";
+            choice = 'N';
+        }
+
 
     } while (choice == 'Y' || choice == 'y');
 
     // Print results
     cout << "\n--- STUDENT SUMMARY ---\n";
-    for (int i = 0; i < names.size(); i++) {
+    for (int i = 0; i < names.size(); i++) 
+    {
         int avg = (math[i] + english[i] + science[i]) / 3;
-        string status = (avg >= 90) ? "Excellent" :
-                        (avg >= 75) ? "Passed" : "Failed";
+        
+        string remarks;
+        if(avg <= 74) remarks = "Failed!";
+        else if(avg <= 89) remarks = "Passed";
+        else remarks = "Excellent";
 
-        cout << names[i] << " - Average: " << avg << " - " << status << endl;
+        cout << names[i] << " - Average: " << avg << " - " << remarks << endl;
     }
-
     return 0;
 }
